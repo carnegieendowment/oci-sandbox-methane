1fc928cad7a03c0ac73d03ea8aaa3f5203cd59be
sawojcicki <sawojcicki@gmail.com>
2017-07-20 16:52:36 -0400

    Update utils.js

--- app/assets/scripts/utils.js
+++ app/assets/scripts/utils.js
@@ -76,50 +76,52 @@ var utils = {
         }
       }
       for (var z = 0; z < 2; z++) {
-        for (var l = 0; l < data.metadata.refinery.split(',').length; l++) {
-          // this for loop is for LPG runs
-          for (var m = 0; m < 2; m++) {
-            // if we don't have the necessary data, load it
-            var prelimRun = 'run' + z + l + m;
+        for (var a = 0; a < 2; a++) {
+          for (var l = 0; l < data.metadata.refinery.split(',').length; l++) {
+            // this for loop is for LPG runs
+            for (var m = 0; m < 2; m++) {
+              // if we don't have the necessary data, load it
+              var prelimRun = 'run' + z + a + l + m;
 
-            if (!Oci.Collections.prelim.get(prelimRun)) {
-              var prelimModel = new PrelimModel({ id: prelimRun });
-              prelimModel.fetch({ async: false, success: function (data) {
-                Oci.Collections.prelim.add(data);
-              }});
+              if (!Oci.Collections.prelim.get(prelimRun)) {
+                var prelimModel = new PrelimModel({ id: prelimRun });
+                prelimModel.fetch({ async: false, success: function (data) {
+                  Oci.Collections.prelim.add(data);
+                }});
+              }
+
+              var prelim = Oci.Collections.prelim.get(prelimRun).toJSON()[key];
+              // we might not have a prelim run for this oil (certain oils don't
+              // run through some refineries)
+              if (!prelim) break;
+
+              [0, 0.5, 1].forEach(function (showCoke) {
+                var refining = +utils.getRefiningTotal(prelim);
+                var combustion = +utils.getCombustionTotal(prelim, showCoke, m);
+
+                // Sum it up! (conditionally based on whether component is selected)
+                var total;
+                components.upstream = opgeeExtent;
+                components.midstream = refining;
+                components.downstream = combustion + transport;
+                if (component) {
+                  total = components[component];
+                } else {
+                  total = _.reduce(components, function (a, b) { return a + b; }, 0);
+                }
+
+                // Handle ratio
+                total = utils.getValueForRatio(total, ratio, prelim, showCoke, data.info[key], m);
+
+                // Check which is bigger (or smaller)
+                if (!opgeeExtent || (extraction * minMaxMultiplier > opgeeExtent * minMaxMultiplier)) {
+                  opgeeExtent = extraction;
+                }
+                if (!extent || (total * minMaxMultiplier > extent * minMaxMultiplier)) {
+                  extent = total;
+                }
+              });
             }
-
-            var prelim = Oci.Collections.prelim.get(prelimRun).toJSON()[key];
-            // we might not have a prelim run for this oil (certain oils don't
-            // run through some refineries)
-            if (!prelim) break;
-
-            [0, 0.5, 1].forEach(function (showCoke) {
-              var refining = +utils.getRefiningTotal(prelim);
-              var combustion = +utils.getCombustionTotal(prelim, showCoke, m);
-
-              // Sum it up! (conditionally based on whether component is selected)
-              var total;
-              components.upstream = opgeeExtent;
-              components.midstream = refining;
-              components.downstream = combustion + transport;
-              if (component) {
-                total = components[component];
-              } else {
-                total = _.reduce(components, function (a, b) { return a + b; }, 0);
-              }
-
-              // Handle ratio
-              total = utils.getValueForRatio(total, ratio, prelim, showCoke, data.info[key], m);
-
-              // Check which is bigger (or smaller)
-              if (!opgeeExtent || (extraction * minMaxMultiplier > opgeeExtent * minMaxMultiplier)) {
-                opgeeExtent = extraction;
-              }
-              if (!extent || (total * minMaxMultiplier > extent * minMaxMultiplier)) {
-                extent = total;
-              }
-            });
           }
         }
       }
@@ -637,18 +639,19 @@ var utils = {
   },
 
   // Get the current PRELIM model
-  getPRELIMModel: function (gwp, refinery, lpg) {
+  getPRELIMModel: function (gwp, hydrogen, refinery, lpg) {
     var metadata = Oci.data.metadata;
     var zi = Number(gwp);
     var ri = this.trimMetadataArray(metadata.refinery.split(',')).indexOf(refinery);
     var li = Number(lpg);
+    var ai = Number(hydrogen);
     // Generate model string
     var model = 'run';
     // If we don't have a match, return default
     if (ri === -1) {
-      model += (zi + '0' + li);
+      model += (zi + ai + '0' + li);
     } else {
-      model = model + zi + ri + li;
+      model = model + zi + ai + ri + li;
     }
     return model;
   },
